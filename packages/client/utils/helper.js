452fa649b487092f8bca14252a9072354d37242e
recaicosar <info@recaicosar.com>
2022-06-17 14:39:52 +0300

    storage reset button added

--- packages/client/utils/helper.js
+++ packages/client/utils/helper.js
@@ -1,6 +1,9 @@
 export const storageCheck = (key) =>
   typeof window !== "undefined" ? localStorage.getItem(key) : null;
 
+  export const storageRemove = (key) =>
+  typeof window !== "undefined" ? localStorage.removeItem(key) : null;
+
 export const uuidv4 = () =>
   ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
     (
